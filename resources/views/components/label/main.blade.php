18779c53cc8777652e30ba74b5bbc6698df5aad5
Pranay Baddam <pranayb2773@gmail.com>
2023-11-25 15:04:03 +0000

    Added link button component and made changes to label component

--- resources/views/components/label/main.blade.php
+++ resources/views/components/label/main.blade.php
@@ -1,8 +1,8 @@
 @props(['value', 'required' => false])
 
-<label {{ $attributes->twMerge(['class' => 'block font-medium text-sm text-secondary-700 dark:text-secondary-300']) }}>
+<label {{ $attributes->twMerge(['class' => 'block font-medium text-sm leading-6 text-secondary-700 dark:text-secondary-300']) }}>
     {{ $value ?? $slot }}
     @if ($required)
-        <sup class="text-danger-600">*</sup>
+        <sup class="text-danger-600 font-bold text-xs">*</sup>
     @endif
 </label>
